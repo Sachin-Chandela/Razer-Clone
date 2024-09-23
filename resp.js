bceec607c5cba9bd34e8706dd2ba6204c06d7672
Sachin-Chandela <150685218+Sachin-Chandela@users.noreply.github.com>
2024-09-23 20:10:41 +0530

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,10 @@
+burger = document.querySelector('.burger');
+header2 = document.querySelector('.header2');
+
+
+
+
+burger.addEventListener('click', ()=>{
+    console.log('hello world ')
+header2.classList.toggle('vclassresp');
+})
